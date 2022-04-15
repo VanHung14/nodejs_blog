b5f7b9ab382758cbfae026ecfa05b5ce33e59050
VanHung14 <dinhvanhung173@gmail.com>
2022-04-15 23:37:57 +0700

    Refactor code: create routes, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -7,10 +7,17 @@ const { dirname } = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({ // xu ly du lieu submit duoi dang form len
+  extended: true
+})) 
+app.use(express.json()) // xu ly gui du lieu tu javascript len
+
 // http logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 // template engine
 app.engine('hbs', handlebars.engine({
@@ -18,17 +25,10 @@ app.engine('hbs', handlebars.engine({
 }));
 app.set('view engine', 'hbs'); 
 app.set('views', path.join(__dirname, 'resources\\views'));  
-// console.log(path.join(__dirname, 'resources\\views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-    
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-    
-})
+// route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
